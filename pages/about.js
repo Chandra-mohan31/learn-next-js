b08040ab82e83bb8d8fcb1a70628be883d2c4ac2
Chandra Mohan.S <chandrashanmugam007@gmail.com>
2021-05-21 11:40:27 +0530

    setup

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+
+
+function about() {
+    return (
+        <div>
+        <Head>
+          <title>About page</title>
+        </Head>
+          hey about  
+
+        </div>
+    )
+}
+
+export default about;
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+    <Head>
+      <title>
+        Web devs bro
+      </title>
+      <meta name='keywords' content='web devolepment' />
+    </Head>
+    <h1>hey there</h1>
+    </div>
+  )
+}
